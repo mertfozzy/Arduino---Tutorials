2a55ac2cd9ec86f99b4bfff0575cd84ce84e9f17
Mert Altuntaş <mertfozzy@gmail.com>
2020-07-10 22:05:53 +0300

    Add files via upload

---
+++ Arduino_7/Arduino_7.ino
@@ -0,0 +1,22 @@
+#define led 3
+
+void setup() {
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int isik = analogRead(A0);
+  Serial.println(isik);
+  delay(50);
+
+  if (isik>560){
+    digitalWrite(led,LOW);
+  }
+  if (isik<560){
+    digitalWrite(led,HIGH);
+  }
+ 
+}
