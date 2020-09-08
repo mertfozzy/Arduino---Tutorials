57d8a64c4548d72285e52f1c4a6d8d10493e4c5a
Mert Altuntaş <mertfozzy@gmail.com>
2020-09-08 09:37:32 +0300

    Add files via upload

---
+++ RFID - Servo Control/rfid.ino
@@ -0,0 +1,56 @@
+#include <MFRC522.h>
+#include <Servo.h>
+#include <SPI.h>
+
+int RST_PIN = 9;
+int SS_PIN = 10;
+int servoPin = 8;
+
+Servo motor;
+MFRC522 rfid(SS_PIN, RST_PIN);
+byte ID[4] = {42,5,215,129};
+
+void setup (){
+  motor.attach(servoPin);
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+  
+}
+
+void loop (){
+  if(! rfid.PICC_IsNewCardPresent())
+  return;
+  if(! rfid.PICC_ReadCardSerial())
+  return;
+
+  if (rfid.uid.uidByte[0] == ID[0] && 
+  rfid.uid.uidByte[1] == ID[1] &&
+  rfid.uid.uidByte[2] == ID[2] && 
+  rfid.uid.uidByte[3] == ID[3]){
+    
+
+Serial.println("Kapi Acildi");
+ekranaYazdir();
+motor.write(180);
+delay(3000);
+motor.write(0);
+delay(1000);
+
+    
+  }
+  else{
+    Serial.println("Yetkisiz Kart");
+    ekranaYazdir();
+    rfid.PICC_HaltA();
+  }
+}
+
+void ekranaYazdir(){
+  Serial.println("ID Numarası :");
+  for(int sayac=0; sayac < 4; sayac++){
+    Serial.println(rfid.uid.uidByte[sayac]);
+    
+  }
+
+}
